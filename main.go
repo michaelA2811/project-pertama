73693ea7fc0ea015195a108eb4cac2ed814b73f0
Michael Ardhyanto <michael@axion.ltd>
2022-06-27 10:33:27 +0700

    odd even

--- main.go
+++ main.go
@@ -5,5 +5,13 @@ import "fmt"
 func main() {
 	// var name string = "John"
 	// var age int = 25
-	fmt.Println("hello world")
+	// fmt.Println("hello world")
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Println("Genap")
+		} else {
+			fmt.Println("Ganjil")
+		}
+
+	}
 }
