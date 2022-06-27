d0b54022ce03a22ce76184288648b6ca6c38fdd4
Michael Ardhyanto <michael@axion.ltd>
2022-06-27 10:44:09 +0700

    slice name

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ func main() {
 	// var name string = "John"
 	// var age int = 25
 	// fmt.Println("hello world")
-	for i := 0; i < 10; i++ {
-		if i%2 == 0 {
-			fmt.Println("Genap")
-		} else {
-			fmt.Println("Ganjil")
-		}
+	// for i := 0; i < 10; i++ {
+	// 	if i%2 == 0 {
+	// 		fmt.Println("Genap")
+	// 	} else {
+	// 		fmt.Println("Ganjil")
+	// 	}
 
+	// }
+	nama := []string{"Budi", "Anies", "John", "Doe", "Lorem"}
+	for i := 0; i < len(nama); i++ {
+		fmt.Println(nama[i])
 	}
 }
