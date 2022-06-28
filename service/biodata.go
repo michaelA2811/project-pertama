7255311603c780cc9631eafb616622c0b655c59a
Michael Ardhyanto <michael@axion.ltd>
2022-06-28 18:00:47 +0700

    comment biodata

--- service/biodata.go
+++ service/biodata.go
@@ -1,41 +1,40 @@
-package main
+package service
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
+// import (
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// )
 
-type Biodata struct {
-	name    string
-	address string
-	job     string
-	reason  string
-}
+// type Biodata struct {
+// 	name    string
+// 	address string
+// 	job     string
+// 	reason  string
+// }
 
-func main() {
-	var i int
-	checkEmployee(i)
-}
+// func main() {
+// 	var i int
+// 	checkEmployee(i)
+// }
 
-func checkEmployee(i int) {
-	var employee = []struct {
-		person Biodata
-	}{
-		{person: Biodata{name: "Budi", address: "Jl.Semangka", job: "Software Developer", reason: "Butuh uang"}},
-		{person: Biodata{name: "Truno", address: "Jl.Pepaya", job: "Backend Developer", reason: "Butuh uang"}},
-		{person: Biodata{name: "Aldo", address: "Jl.Pisang", job: "Human Resource", reason: "Butuh uang"}},
-		{person: Biodata{name: "Sodikin", address: "Jl.Kejujuran", job: "Frontend Developer", reason: "Butuh uang"}},
-	}
-	tempArgs := os.Args
-	if len(tempArgs) == 2 {
-		i, err := strconv.Atoi(tempArgs[1])
-		if err != nil {
-			fmt.Println("angkanya error")
-			return
-		}
-		fmt.Printf("%+v\n", employee[i])
-	}
-	// fmt.Println(os.Args)
-	// fmt.Println(employee[len(os.Args)-1])
-}
+// func checkEmployee(i int) {
+
+// 	var employee = []Biodata{
+// 		{name: "Budi", address: "Jl.Semangka", job: "Software Developer", reason: "Butuh uang"},
+// 		{name: "Truno", address: "Jl.Pepaya", job: "Backend Developer", reason: "Butuh uang"},
+// 		{name: "Aldo", address: "Jl.Pisang", job: "Human Resource", reason: "Butuh uang"},
+// 		{name: "Sodikin", address: "Jl.Kejujuran", job: "Frontend Developer", reason: "Butuh uang"},
+// 	}
+// 	tempArgs := os.Args
+// 	if len(tempArgs) == 2 {
+// 		i, err := strconv.Atoi(tempArgs[1])
+// 		if err != nil {
+// 			fmt.Println("angkanya error")
+// 			return
+// 		}
+// 		fmt.Printf("%+v\n", employee[i])
+// 	}
+// 	// fmt.Println(os.Args)
+// 	// fmt.Println(employee[len(os.Args)-1])
+// }
